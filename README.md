b3d9f663010be1053011dd79d1869bb6f626a8f4
mvishu405 <mvishu405@gmail.com>
2020-02-04 12:54:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-7ewwgs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-7ewwgs)
---
+++ index.js
@@ -0,0 +1,22 @@
+function selectionSort(arr)
+{
+  let arrLength = arr.length;
+  let i,j,min_index;
+  for(i = 0; i < arrLength - 1; i++)
+  {
+    min_index = i;
+    for(j = i+1; j < arrLength; j++)
+    {
+      if (arr[j] < arr[min_index]) 
+      {
+        min_index = j;
+        }
+    }
+  }
+}
+
+let arr = [60,25,30,12,35,-10];
+
+selectionSort(arr);
+
+console.log(arr);
