91d5cd1822c7d6454dfd8b323402a0b320291482
mvishu405 <mvishu405@gmail.com>
2020-02-12 14:58:49 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-function selectionSort(arr)
-{
+function selectionSort(arr) {
   let arrLength = arr.length;
-  let i,j,min_index;
-  for(i = 0; i < arrLength - 1; i++)
-  {
+  let i, j, min_index;
+  for (i = 0; i < arrLength - 1; i++) {
     min_index = i;
-    for(j = i+1; j < arrLength; j++)
-    {
-      if (arr[j] < arr[min_index]) 
-      {
+    for (j = i + 1; j < arrLength; j++) {
+      if (arr[j] < arr[min_index]) {
         min_index = j;
-        }
+      }
     }
   }
 }
 
-let arr = [60,25,30,12,35,-10];
+let arr = [60, 25, 30, 12, 35, -10];
 
 selectionSort(arr);
 
